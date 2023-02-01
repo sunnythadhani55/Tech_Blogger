13161aec9747c053d110b6d04aa88437a9db3d47
sunnythadhani55 <123536034+sunnythadhani55@users.noreply.github.com>
2023-02-01 16:43:41 +0530

    Add missing custom exception class "UnauthorizedException"

---
+++ tech_blogger/src/main/java/com/blogger/tech/exception/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.blogger.tech.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+  public UnauthorizedException(String resourceName, String propertyName, String propertyValue) {
+    super(String.format("User is unauthorized to  access %1$s with %2$s %3$s", resourceName,
+        propertyName, propertyValue));
+  }
+
+}
